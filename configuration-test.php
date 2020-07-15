fd84109066ceec44ca40ec2903d9b7b2989ffcfd
Nicolas Maillet <n.maillet07@gmail.com>
2020-07-15 14:05:15 +0200

    test configuration and saving

--- configuration-test.php
+++ configuration-test.php
@@ -7,7 +7,7 @@
 	<meta charset="utf-8"/>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 	<link rel="stylesheet" href="css/form.css">
-	<title> Verbes irrégulier - Anglais </title> 
+	<title> Configurer le test </title> 
 </head> 
 <body>
 <?php
@@ -15,13 +15,12 @@
     if(isset($_SESSION['id'])) {
         ?>
         <div class="container">
-
-            <form method="post" action="" class="offset-md-4 col-md-4 form">
+            <form method="post" action="test.php" class="offset-md-4 col-md-4 form">
                 <legend>Configurer le test</legend>
 
                 <label for="number">Nombre de verbe à réviser :</label>
-                <input type="number" name="number" id="number" min="10" max="170" class="form-control">
-
+                <input type="number" name="number" id="number" min="10" max="170" class="form-control" required>
+                <br>    
                 <p> Quels verbes voulez-vous réviser ? </p>
 
                 <input type="checkbox" name="learned[]" id="pas_appris" value="0"> 
@@ -30,13 +29,18 @@
                 <label for="en_cours">En cours</label> <br>
                 <input type="checkbox" name="learned[]" id="appris" value="2" checked="#"> 
                 <label for="appris">Appris</label> <br>
-
+                <br>
                 <input type="submit" value="Lancer le test" class="btn btn-info">
+                <br>
+                <small><a href="verb-list.php">Annuler</a></small>
             </form>
-
         </div>
 
     <?php
     } else {
         header("Location:index.php");
     }
+    ?>
+
+</body>
+</html>
--- save-test.php
+++ save-test.php
@@ -5,67 +5,81 @@
 <html>
 <head>
 	<meta charset="utf-8"/>
-	<link rel="stylesheet" href="css/bootstrap.css">
-	<link rel="stylesheet" href="css/connexion.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/form.css">
 	<title> Sauvegarder le Test </title> 
 </head> 
 <body>
 <?php
 
-    if(isset($_POST['number']) && isset($_POST['learn']) && isset($_POST['result']) ) {
-        
-        $number = $_POST['number'];
-        $returnLearn = $_POST['learn'];
-        $result = $_POST['result'];
+    if(isset($_SESSION['id'])) {
 
-        switch($returnLearn) {
-			case "0" :
-				$learn = 0;
-				break;
-			case "1" :
-				$learn = 1;
-				break;
-			case "2" :
-				$learn = 2;
-				break;
-			case "0,1" :
-				$learn = 10;
-				break;
-			case "0,2" :
-				$learn = 20;
-				break;
-			case "1,2" :
-				$learn = 12;
-				break;
-			case "'0','1','2'" :
-				$learn = 120;
-				break;
-			default : 
-				$learn = 120;		
-		}
+        if(isset($_POST['number']) && isset($_POST['learn']) && isset($_POST['result'])) {
+            
+            $number = $_POST['number'];
+            $returnLearn = $_POST['learn'];
+            $result = $_POST['result'];
 
-        // Database connection 
-        require('database-connection.php');
+            switch($returnLearn) {
+                case "0" :
+                    $learn = 0;
+                    break;
+                case "1" :
+                    $learn = 1;
+                    break;
+                case "2" :
+                    $learn = 2;
+                    break;
+                case "0,1" :
+                    $learn = 10;
+                    break;
+                case "0,2" :
+                    $learn = 20;
+                    break;
+                case "1,2" :
+                    $learn = 12;
+                    break;
+                case "'0','1','2'" :
+                    $learn = 120;
+                    break;
+                default : 
+                    $learn = 120;		
+            }
 
-        // Use the actual time in the good format
-        $date = date("Y-m-d H:i:s");
-        $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+            // Database connection 
+            require('database-connection.php');
 
-        $saveTest = $database->prepare("
-        INSERT INTO test(date, number, learn, result, id_user) 
-        VALUES(:date, :number, :learn, :result, :idUser)
-        ");
-        $saveTest->execute(array(
-            ":date" => $date,
-            ":number" => $number,
-            ":learn" => $learn,
-            ":result" => $result,
-            ":idUser" => $_SESSION['id']
-        ));
+            // Use the actual time in the good format
+            $date = date("Y-m-d H:i:s");
 
-        echo $_SESSION['id'];
+            $saveTest = $database->prepare("
+            INSERT INTO test(date, number, learn, result, id_user) 
+            VALUES(:date, :number, :learn, :result, :idUser)
+            ");
+            $saveTest->execute(array(
+                ":date" => $date,
+                ":number" => $number,
+                ":learn" => $learn,
+                ":result" => $result,
+                ":idUser" => $_SESSION['id']
+            ));
+            ?>			
+            <p class="message"> Votre test a bien été enregistré ! </p>
+            <div class="col text-center">
+                <button class="btn btn-info align-self-center return-button"><a href="test-records.php" class="return-link"> Voir mes test </a></button>
+                <button class="btn btn-info align-self-center return-button"><a href="index.php" class="return-link"> Accueil </a></button>
+            </div>			
+            <?php
+
+        } else {
+            ?>
+            <p class="message"> Désolé, une erreur est survenue, votre test n'a pas pu être enregistré. </p>
+            <div class="col text-center">
+                <button class="btn btn-info align-self-center return-button"><a href="index.php" class="return-link"> Accueil </a></button>
+            </div>
+        <?php    
+        }
 
     } else {
-
-
+        header("Location:index.php");
     }
